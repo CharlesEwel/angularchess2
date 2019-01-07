f855df01bd8e0b0d911fc66b997a210e8487b51e
Charles Ewel <charles.ewel@dealers1.com>
2019-01-07 00:44:38 -0800

    commented out scripts

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -10,10 +10,10 @@ export class HistoryComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    basicTabulate('#first-table', jsondata, ['Name', 'Moves', 'Games Found', 'Win', 'Draw', 'Loss', 'Difference', 'Percentage Popularity']); // 2 column table
-    advancedTabulate('#second-table', jsondata);
-    createBasicChart('#first-chart', jsondata);
-    createAdvancedChart('#second-chart', jsondata);
+    // basicTabulate('#first-table', jsondata, ['Name', 'Moves', 'Games Found', 'Win', 'Draw', 'Loss', 'Difference', 'Percentage Popularity']); // 2 column table
+    // advancedTabulate('#second-table', jsondata);
+    // createBasicChart('#first-chart', jsondata);
+    // createAdvancedChart('#second-chart', jsondata);
   }
 
 }
