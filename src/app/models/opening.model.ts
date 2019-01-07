3a96bd252457f1f7f16bd110ba4867b64580b861
Charles Ewel <charles.ewel@dealers1.com>
2019-01-07 12:15:14 -0800

    add opening model

---
+++ src/app/models/opening.model.ts
@@ -0,0 +1,3 @@
+export class Opening {
+  constructor(public name: string, public category: string, public moves: string, public gamesFound: number, public win: number, public draw: number, public loss: number) { }
+}
